1f163a90c5077079b80d15a5c24b225a5bb4b61f
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-03-07 02:24:21 -0600

    Cosmetic changes and keeping the code consistent

--- Assignment3/register.js
+++ Assignment3/register.js
@@ -2,6 +2,7 @@
 var $ = function(id) { return document.getElementById(id); };
 
 var processEntries = function() {
+
 	var isValid = true;
 	// get values for user entries   
     var email = $("email_address").value;
@@ -10,51 +11,59 @@ var processEntries = function() {
     var terms = $("terms").checked;
 
 	// check user entries for validity
-    if (email == "") {
+    if ( email == "" ) {
         $("email_address").nextElementSibling.firstChild.nodeValue = "This field is required.";
 		isValid = false;
     }
 	else {
 		$("email_address").nextElementSibling.firstChild.nodeValue = "";	
 	}
-	if (phone == "") {
+
+	if ( phone == "" ) {
         $("phone").nextElementSibling.firstChild.nodeValue = "This field is required.";
 		isValid = false;
     }
 	else {
 		$("phone").nextElementSibling.firstChild.nodeValue = "";
 	}
-    if (country == "") {
+
+    if ( country == "" ) {
         $("country").nextElementSibling.firstChild.nodeValue = "Please select a country.";
 		isValid = false;
     }
 	else {
 		$("country").nextElementSibling.firstChild.nodeValue = "";	
 	}
-	if (terms == false) {
+
+	if ( terms == false ) {
         $("terms").nextElementSibling.firstChild.nodeValue = "This box must be checked.";
 		isValid = false;
     }
 	else {
 		$("terms").nextElementSibling.firstChild.nodeValue = "";	
 	}
-    if(isValid)
-	{
+
+    if( isValid ) {
 		$("registration_form").submit();
 	}
+
 };
 
 var resetForm = function() {
+
     $("registration_form").reset();
 	$("email_address").nextElementSibling.firstChild.nodeValue = "*";
 	$("phone").nextElementSibling.firstChild.nodeValue = "*";	
 	$("country").nextElementSibling.firstChild.nodeValue = "*";	
 	$("terms").nextElementSibling.firstChild.nodeValue = "*";
     $("email_address").focus();
+
 };
 
 window.onload = function() {
+
     $("register").onclick = processEntries;
     $("reset_form").onclick = resetForm;    
     $("email_address").focus();
+
 };
